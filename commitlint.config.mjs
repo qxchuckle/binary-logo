6a5518d4ca175f093a06a90bdd1b539540e9365d
qxchuckle <1934009145@qq.com>
2024-10-10 19:50:21 +0800

    feat(custom): 修改commit type

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,4 +3,24 @@ import { defineConfig } from '@archoleat/commitlint-define-config';
 
 export default defineConfig({
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'init',
+      ],
+    ],
+  },
 });
